a61e7c0cf74573a8697e1470fcfaeb6299ce4df1
Albert Vesker <16742466+ivrm@users.noreply.github.com>
2020-07-25 11:40:34 +0300

    Update gpedit.bat

--- gpedit.bat
+++ gpedit.bat
@@ -1,7 +1,7 @@
 @echo off
 dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >find-gpedit.txt
 dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>find-gpedit.txt
-echo Ustanovka gpedit.msc
+echo Install gpedit.msc
 for /f %%i in ('findstr /i . find-gpedit.txt 2^>nul') do dism /online /norestart /add-package:"C:\Windows\servicing\Packages\%%i"
-echo Gpedit ustanovlen.
+echo Gpedit installed.
 pause
